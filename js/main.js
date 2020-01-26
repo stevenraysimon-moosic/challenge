965045d4f189c4eab55370fce0273df7ce8b3d9c
stevenraysimon-moosic <59861359+stevenraysimon-moosic@users.noreply.github.com>
2020-01-26 00:34:24 -0500

    updating song number

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@ function duration(v)
         var minutes = parseInt(audio.duration / 60, 10);
         var seconds = parseInt(audio.duration % 60);
         document.getElementById("song"+v).innerHTML = minutes+":"+seconds;
-        document.getElementById("songTitle"+v).innerHTML = v+'. Casper';
+        document.getElementById("songTitle"+v).innerHTML = (v+1)+'. Casper';
 
         //Calculate total song duration
         totalDuration = totalDuration + audio.duration;
